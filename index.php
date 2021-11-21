e9a78c3f224fcff7fbae68f77d0e63f1950d8526
AntonySanjay <94779257+AntonySanjay@users.noreply.github.com>
2021-11-21 13:44:55 +0530

    Add files via upload
    initialStage

---
+++ index.php
@@ -0,0 +1,62 @@
+<html>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;1,500&display=swap" rel="stylesheet">
+
+
+<body>
+    <div class="heading">
+        <h1>Loyola Hostel</h1>
+        <img src="https://scontent.fmaa2-1.fna.fbcdn.net/v/t1.18169-1/p200x200/15895109_1224474727634252_2699928315332695185_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=dbb9e7&_nc_ohc=WNFRYo9FrPcAX-nDjU_&_nc_ht=scontent.fmaa2-1.fna&oh=6f0a9b2a4168fb9fb4ba476a006013af&oe=61946E47" alt="">
+    </div>
+    <form action="messbill.php" method="post">
+        <label>Enter Account Number</label>
+        <input type="number" name="account_number"><BR><BR>
+        <label>Enter Student Name</label>
+        <input type="text" name="student_name"><BR><BR>
+        <label>Enter Days </label>
+        <input type="number" name="total_days"><BR><BR>
+        <label>Choose Mess</label>
+        <select name="mess">
+            <option value="VEG">VEG</option>
+            <option value="NON-VEG">NON-VEG</option>
+        </select>
+        <input class="button" type="submit" name="save" value="Calculate">
+    </form>
+</body>
+
+<style>
+    * {
+        font-family: 'Poppins', sans-serif;
+    }
+
+    .heading {
+        text-align: center;
+        margin: 20px 0;
+        color: blue;
+    }
+
+    .button {
+        margin-top: 30px;
+    }
+
+    label  {
+        font-size: 18px;
+    }
+
+    label::after {
+        content: ":";
+    }
+
+    form {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        max-width: 20%;
+        margin: 0 auto;
+        margin-bottom: 40px;
+    }
+</style>
+
+</html>
---
+++ messbill.php
@@ -0,0 +1,35 @@
+<?php
+$server_name = "localhost";
+$username = "root";
+$password = "";
+$database_name = "mini";
+$conn = mysqli_connect($server_name, $username, $password, $database_name);
+if (!$conn) {
+    die("connection failed" . mysqli_connect_error());
+}
+
+if (isset($_POST['save'])) {
+
+    $account_number = $_POST['account_number'];
+    $student_name = $_POST['student_name'];
+    $total_days = $_POST['total_days'];
+    $amount = 0;
+    $mess = $_POST['mess'];
+
+
+    if ($mess == "VEG") {
+        $amount = $total_days * 100;
+    } else if ($mess == "NON-VEG") {
+        $amount = $total_days * 150;
+    }
+
+    $sql_query = "INSERT INTO `mess` (`account_number`, `student_name`, `total_days`, `mess`, `bill`) VALUES ('$account_number','$student_name','$total_days','$mess','$amount')";
+
+
+    if (mysqli_query($conn, $sql_query) ) {
+        echo "Mess Bill=Rs." . $amount;
+    } else {
+        echo "error";
+    }
+    mysqli_close($conn);
+}
